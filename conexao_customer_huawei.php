417be1eed14bc75ba92c5c670a296881de7e13f5
alexandrenassil <57010402+alexandrenassil@users.noreply.github.com>
2021-06-17 17:01:09 -0300

    Update conexao_customer_huawei.php

--- conexao_customer_huawei.php
+++ conexao_customer_huawei.php
@@ -1,6 +1,6 @@
 <?php
 
-$conector = mssql_connect("10.100.0.72", "usr_desenvolvimento", "d3s3nv0lv1m3nt0ti01") or die("NÃO FOI POSSÍVEL A CONEXÃO COM O SERVIDOR");
+$conector = mssql_connect("10.100.0.00", "usuario", "senha") or die("NÃO FOI POSSÍVEL A CONEXÃO COM O SERVIDOR");
 $conn = mssql_select_db("CUSTOMER_NCM", $conector) or die("NÃO FOI POSSÍVEL SELECIONAR O BANCO DE DADOS");
 
 ?>
